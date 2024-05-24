70d07e7d001df59c33828ae9f441ae548e3de1c4
Khairul Basar <khairulbashar24853@gmail.com>
2024-05-24 12:01:59 +0600

    Update and Delete Button added

--- 3_BookForm_for_adding_deleting_item/index.php
+++ 3_BookForm_for_adding_deleting_item/index.php
@@ -12,20 +12,20 @@ $json = file_get_contents('books.json');
 $books = json_decode($json,true); 
 
 // Adding new item to the array
-$books[] =array('title' => 'The Catcher in the Rye',
-    'author' => 'J.D. Salinger',
-    'available' => true,
-    'pages' => 224,
-    'isbn' => '9780316769488');
+// $books[] =array('title' => 'The Catcher in the Rye',
+//     'author' => 'J.D. Salinger',
+//     'available' => true,
+//     'pages' => 224,
+//     'isbn' => '9780316769488');
     
-echo count($books);
+// echo count($books);
 
- $json = json_encode($books, JSON_PRETTY_PRINT);
- file_put_contents("books.json", $json);
+//  $json = json_encode($books, JSON_PRETTY_PRINT);
+//  file_put_contents("books.json", $json);
 
 
 echo "<h2><center>Book Table</center></h2>";
-echo "<center>,<table border='1', summary='This table displays'> 
+echo "<center><table border='1', summary='This table displays'> 
     <tr>
         <th>Title</th>
         <th>Author</th>
@@ -42,6 +42,8 @@ foreach($books as $book)
         echo"    <td>".$book["available"]."</td>";
         echo"    <td>".$book["pages"]."</td>";
         echo"    <td>".$book["isbn"]."</td>";
+        echo "<td><form method=\"post\" action=\"/3_BookForm_for_adding_deleting_item/update.php \"><button type=\"submit\" name=\"update\">Update</button></form></td>";
+        echo "<td><form method=\"post\" action=\"/1_Test \"><button type=\"submit\" name=\"delete\">Delete</button></form></td>";
         echo"</ tr>";
 
 }
---
+++ 3_BookForm_for_adding_deleting_item/update.php
@@ -0,0 +1,23 @@
+<?php
+
+
+echo"<h2><center>Update an item in Table</center></h2>";
+
+echo"<form><center>
+    <label for=\"title\">Title:</label>
+    <input type=\"text\" id=\"title\" name=\"title\"><br><br>
+    <label for=\"author\">Author:</label>
+    <input type=\"text\" id=\"author\" name=\"author\"><br><br>
+    <label for=\"available\">Available:</label>
+    <input type=\"text\" id=\"available\" name=\"available\"><br><br>
+    <label for=\"pages\">Pages:</label>
+    <input type=\"text\" id=\"pages\" name=\"pages\"><br><br>
+    <label for=\"isbn\">ISBN:</label>
+    <input type=\"text\" id=\"isbn\" name=\"isbn\"><br><br>
+    <button type=\"submit\" name=\"edited\">Update</button></center></form>";
+
+
+
+
+
+?>
